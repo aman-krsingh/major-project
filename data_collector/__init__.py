d833c9225de5ba9da2b8cebaa2820d860a44e97f
aman kr singh <91698030+aman-krsingh@users.noreply.github.com>
2024-05-13 22:02:11 +0530

    Update __init__.py

--- data_collector/__init__.py
+++ data_collector/__init__.py
@@ -2,35 +2,34 @@ from io import BytesIO
 from azure.storage.filedatalake import DataLakeServiceClient
 from azure.identity import DefaultAzureCredential
 
-
 import logging
-
 from azure.functions import TimerRequest
+
 from datetime import datetime, timezone
 
 from alpha_vantage.timeseries import TimeSeries
 
 
-
 def main(mytimer: TimerRequest) -> None:
     utc_timestamp = datetime.utcnow().replace(tzinfo=timezone.utc).isoformat()
+    
+    current_datetime = datetime.now()
+    current_date = current_datetime.strftime("%Y-%m-%d")
+    
     ticker_list = ['AAPL', 'META', 'GOOG']
-
     API_key = 'WSKF50ODKWY4WP1O'
+    
     #storage acount address
     account_url = f"https://storageaacount456.dfs.core.windows.net/"
     token_credential = DefaultAzureCredential()
 
     #service clint bana dega data lake ka
     service_client = DataLakeServiceClient(account_url, credential=token_credential)
-    filesystem_client = service_client.get_file_system_client(file_system="stocks-data")
-    directory_client = filesystem_client.get_directory_client("dir")
-    file_client = directory_client.get_file_client("")
-    
+    filesystem_client = service_client.get_file_system_client(file_system="stocks-data")    
 
     for ticker in ticker_list:
         directory_client = filesystem_client.get_directory_client(f"data/{ticker}")
-        file_client = directory_client.get_file_client(f"{ticker}")
+        file_client = directory_client.get_file_client(f"{ticker}_{current_date}.csv")
         ts = TimeSeries(key= API_key, output_format='pandas')
         res = ts.get_daily(ticker, outputsize='full')
         df=res[0]
