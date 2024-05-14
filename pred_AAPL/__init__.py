7d6cd7f9ea169d08841f33140d8b9fc8a83bc898
aman kr singh <91698030+aman-krsingh@users.noreply.github.com>
2024-05-15 01:22:48 +0530

    Create __init__.py

---
+++ pred_AAPL/__init__.py
@@ -0,0 +1,51 @@
+from io import BytesIO
+from azure.storage.filedatalake import DataLakeServiceClient
+from azure.identity import DefaultAzureCredential
+
+import tempfile
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+import pandas as pd
+import math 
+from numpy import array
+from datetime import datetime
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import LSTM
+
+from sklearn.metrics import mean_squared_error
+
+import logging
+
+from azure.functions import HttpRequest, HttpResponse
+
+
+
+
+
+def main(req: HttpRequest) -> HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+
+
+    
+    if name:
+        return HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return HttpResponse(
+            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+            status_code=200
+        )
